fbc51b3e011b6e49ab7e24b2f6f88d0be143aef1
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-09-23 10:02:37 -0700

    material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useState } from "react";
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+
 
 export default function App() {
-  return (
-    <div>Weather Bandit</div>
-  )
+    return (
+        <>
+            <div>Weather Bandit</div>
+            <div className="main">
+                <h1>React Search</h1>
+                <div className="search">
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        label="Search"
+                    />
+                </div>
+            </div>
+        </>
+    )
 }
