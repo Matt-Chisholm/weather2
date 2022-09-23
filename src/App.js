c7e26632b1c05fdc4294fbfe0c4b569f99107705
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-09-23 10:32:39 -0700

    extract search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import { useState } from "react";
 import React from 'react';
-import TextField from '@mui/material/TextField';
+import LocationSearch from "./LocationSearch";
+import './App.css';
+
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://foreca-weather.p.rapidapi.com/current/106174041',
+  params: {alt: '0', tempunit: 'C', windunit: 'MS', tz: 'Vancouver', lang: 'en'},
+  headers: {
+    'X-RapidAPI-Key': '1412b37728msh10138129aac7035p1f1d48jsn04de011cc0c4',
+    'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 
 
@@ -8,17 +27,7 @@ export default function App() {
     return (
         <>
             <div>Weather Bandit</div>
-            <div className="main">
-                <h1>React Search</h1>
-                <div className="search">
-                    <TextField
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth
-                        label="Search"
-                    />
-                </div>
-            </div>
+           <LocationSearch />
         </>
     )
 }
--- src/LocationSearch.js
+++ src/LocationSearch.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Default from '@mui/material/TextField';
 
 export default function LocationSearch() {
   return (
-    <div></div>
+    <div>
+         <div className="main">
+                <h1>Where do you live?</h1>
+                <div className="search">
+                    <Default
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        label="Search"
+                    />
+                </div>
+            </div>
+    </div>
   )
 }
