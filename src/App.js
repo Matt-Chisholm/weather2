28403456132f7f6d29a4fb7018a88bc24f8dd954
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-09-24 10:20:39 -0700

    url change for api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { useState } from 'react';
 
 
 export default function App() {
+
+
+    const url = 'https://api.weatherbit.io/v2.0/current?city=Victoria&key=e2aadbd0de2f495ebb38ef9fcd744cb0&include=minutely';
+
     return (
         <>
         </>
